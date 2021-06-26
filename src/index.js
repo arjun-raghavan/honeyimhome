d0378603a8298a936a870183e3d3d1e171ad3b59
Gaurav Bhardwaj <gaurav.bhardwaj@cimpress.com>
2021-06-27 00:20:18 +0530

    Added Material UI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+   <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
+  </ThemeProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
